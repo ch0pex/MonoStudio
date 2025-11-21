465d9fb4c19f6cae563c5386d9d5474eb943c58f
chope <100432481@alumnos.uc3m.es>
2025-11-21 17:13:32 +0100

    Add better naming to avoid test collision

--- cmake/modules/testing.cmake
+++ cmake/modules/testing.cmake
@@ -3,9 +3,9 @@ find_package(doctest CONFIG REQUIRED)
 add_custom_target(${PROJECT_NAME}-tests)
 
 function(create_test test_name test_src)
-  add_executable(${test_name} ${test_src})
-  target_link_libraries(${test_name} PRIVATE doctest::doctest ${PROJECT_NAME}-lib)
-  add_test(test_${PROJECT_NAME}_${test_name} ${test_name})
-  add_dependencies(${PROJECT_NAME}-tests ${test_name})
+  add_executable(test_${PROJECT_NAME}_${test_name} ${test_src})
+  target_link_libraries(test_${PROJECT_NAME}_${test_name} PRIVATE doctest::doctest ${PROJECT_NAME}-lib)
+  add_test(test_${PROJECT_NAME}_${test_name} test_${PROJECT_NAME}_${test_name})
+  add_dependencies(${PROJECT_NAME}-tests test_${PROJECT_NAME}_${test_name})
 endfunction()
 
