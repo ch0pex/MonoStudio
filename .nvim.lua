f8e650402c0cf2f0dc8174385d3e7751ae931c8d
ch0pex <acbsur1@gmail.com>
2025-12-03 01:26:31 +0100

    Add more overseer tasks

--- .nvim.lua
+++ .nvim.lua
@@ -10,9 +10,58 @@ overseer.setup({
 })
 
 overseer.register_template({
+	name = "Build: Debug",
+
+	builder = function()
+		return {
+			cmd = { "./scripts/build.sh" },
+			args = { "Debug" },
+			components = {
+				"default",
+				"on_complete_dispose",
+			},
+		}
+	end,
+
+	priority = 100,
+})
+overseer.register_template({
+	name = "Build: Release",
+
+	builder = function()
+		return {
+			cmd = { "./scripts/build.sh" },
+			args = { "Release" },
+			components = {
+				"default",
+				"on_complete_dispose",
+			},
+		}
+	end,
+
+	priority = 100,
+})
+overseer.register_template({
+	name = "Build: RelWithDebInfo",
+
+	builder = function()
+		return {
+			cmd = { "./scripts/build.sh" },
+			args = { "RelWithDebInfo" },
+			components = {
+				"default",
+				"on_complete_dispose",
+			},
+		}
+	end,
+
+	priority = 100,
+})
+
+overseer.register_template({
 	name = "Workflow: Debug",
 
-	builder = function(params)
+	builder = function()
 		return {
 			cmd = { "./scripts/workflow.sh" },
 			args = { "Debug" },
@@ -28,7 +77,7 @@ overseer.register_template({
 overseer.register_template({
 	name = "Workflow: Release",
 
-	builder = function(params)
+	builder = function()
 		return {
 			cmd = { "./scripts/workflow.sh" },
 			args = { "Release" },
@@ -44,7 +93,7 @@ overseer.register_template({
 overseer.register_template({
 	name = "Workflow: RelWithDebInfo",
 
-	builder = function(params)
+	builder = function()
 		return {
 			cmd = { "./scripts/workflow.sh" },
 			args = { "RelWithDebInfo" },
@@ -61,7 +110,7 @@ overseer.register_template({
 overseer.register_template({
 	name = "Workflow: All",
 
-	builder = function(params)
+	builder = function()
 		return {
 			cmd = { "./scripts/full_workflow.sh" },
 			components = {
