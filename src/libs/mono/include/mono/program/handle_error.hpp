e39fd95b917d606773abe82b725b68327e790b5a
ch0pex <acbsur1@gmail.com>
2025-12-16 23:44:35 +0100

    [Mono]: Add better handle error message

--- src/libs/mono/include/mono/program/handle_error.hpp
+++ src/libs/mono/include/mono/program/handle_error.hpp
@@ -14,10 +14,10 @@ inline ex::sender auto handle_error(std::exception_ptr err) {
     std::rethrow_exception(std::move(err));
   }
   catch (std::exception const& e) {
-    LOG_ERROR("An exception was thrown: {}", e.what());
+    LOG_ERROR("A fatal error ocurred during program execution: {}", e.what());
   }
   catch (...) {
-    LOG_ERROR("Unkown error ocurred! Shutting down vixel.");
+    LOG_ERROR("Unkown error ocurred! Shutting down program.");
   }
   ex::request_stop();
   return ex::just();
