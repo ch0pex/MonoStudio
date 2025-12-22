692896a98770a8e9f9bc1f2f195f81fbe91b7d09
ch0pex <acbsur1@gmail.com>
2025-12-22 02:44:40 +0100

    [CMake]: Improve create test macro to support labels and resource locks

--- cmake/modules/testing.cmake
+++ cmake/modules/testing.cmake
@@ -5,13 +5,41 @@ if (NOT EXISTS ${PROJECT_NAME}-tests)
 endif()
 
 function(create_test test_name test_src)
+  set(options "")
+  set(oneValueArgs RESOURCE_LOCK)
+  set(multiValueArgs LABELS)
+  cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
   add_executable(test_${PROJECT_NAME}_${test_name} ${test_src})
   target_link_libraries(test_${PROJECT_NAME}_${test_name} PRIVATE doctest::doctest ${PROJECT_NAME}-lib)
-  add_test(test_${PROJECT_NAME}_${test_name} test_${PROJECT_NAME}_${test_name})
+
+  add_test(NAME test_${PROJECT_NAME}_${test_name}
+           COMMAND test_${PROJECT_NAME}_${test_name})
+
   add_dependencies(${PROJECT_NAME}-tests test_${PROJECT_NAME}_${test_name})
+
+  if(CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+    set_tests_properties(test_${PROJECT_NAME}_${test_name} PROPERTIES
+        WORKING_DIRECTORY "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
+    )
+  endif()
+
+  set(final_labels "${PROJECT_NAME}")
+
+  if(ARG_LABELS)
+    list(APPEND final_labels ${ARG_LABELS})
+  endif()
+
   set_tests_properties(test_${PROJECT_NAME}_${test_name} PROPERTIES
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+      LABELS "${final_labels}"
   )
+  if(ARG_RESOURCE_LOCK)
+    set_tests_properties(test_${PROJECT_NAME}_${test_name} PROPERTIES
+        RESOURCE_LOCK "${ARG_RESOURCE_LOCK}"
+    )
+
+  endif()
+
 endfunction()
 
 function(integration_test test_name test_src)
