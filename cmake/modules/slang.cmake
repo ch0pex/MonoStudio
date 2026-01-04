7bd4c2b55d3467fb65800685de07539aa5bda678
ch0pe <acbsur1@gmail.com>
2026-01-04 22:57:03 +0100

    [Reflect3d]: Add slangc shaders

---
+++ cmake/modules/slang.cmake
@@ -0,0 +1,18 @@
+function(add_slang_shader_target TARGET)
+  cmake_parse_arguments ("SHADER" "" "" "SOURCES" ${ARGN})
+  set (SHADERS_DIR ${CMAKE_CURRENT_LIST_DIR}/assets/shaders)
+  set (ENTRY_POINTS -entry vertMain -entry fragMain)
+  add_custom_command (
+          OUTPUT ${SHADERS_DIR}/bin
+          COMMAND ${CMAKE_COMMAND} -E make_directory ${SHADERS_DIR}
+  )
+  add_custom_command (
+          OUTPUT  ${SHADERS_DIR}/bin/${TARGET}.spv
+          COMMAND slangc ${SHADER_SOURCES} -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name ${ENTRY_POINTS} -o ${SHADERS_DIR}/bin/${TARGET}.spv
+          WORKING_DIRECTORY ${SHADERS_DIR}
+          DEPENDS ${SHADERS_DIR} ${SHADER_SOURCES}
+          COMMENT "Compiling Slang Shaders"
+          VERBATIM
+  )
+  add_custom_target (${TARGET} DEPENDS ${SHADERS_DIR}/bin/${TARGET}.spv)
+endfunction()
---
+++ src/libs/reflect3d/include/reflect3d/graphics/vk/vk_shader_module.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "reflect3d/graphics/vk/utils/vk_native_types.hpp"
+
+#include <cstdint>
+#include <mono/misc/start_lifetime_as.hpp>
+
+#include <filesystem>
+#include <fstream>
+#include <ranges>
+#include <span>
+#include <vector>
+
+namespace rf3d::gfx::vk {
+
+namespace detail {
+
+core::ShaderModuleCreateInfo create_shader_module_create_info(std::ranges::contiguous_range auto const& bytecode) {
+  auto const bytecode_size = bytecode.size() / sizeof(std::uint32_t);
+  core::ShaderModuleCreateInfo create_info {
+    .codeSize = bytecode.size(),
+    .pCode    = mono::start_lifetime_as_array<std::uint32_t const>(bytecode.data(), bytecode_size),
+  };
+
+  return create_info;
+}
+
+} // namespace detail
+
+class Shader {
+public:
+  using byte_code_type = std::vector<char>;
+  using module_type    = raii::ShaderModule;
+  using device_type    = raii::Device;
+
+  explicit Shader(device_type const& device, byte_code_type&& code) :
+    code(std::move(code)), handle(device, detail::create_shader_module_create_info(code)) { }
+
+  [[nodiscard]] module_type const& module() const noexcept { return handle; }
+
+  [[nodiscard]] std::span<std::byte const> bytecode() const noexcept {
+    return std::as_bytes(std::span {code.data(), code.size()});
+  }
+
+private:
+  byte_code_type code;
+  module_type handle;
+};
+
+
+Shader::byte_code_type load_shader_bytecode(std::filesystem::path const& file_path) {
+  std::ifstream file(file_path, std::ios::binary | std::ios::ate);
+
+  if (not file.is_open()) {
+    throw std::runtime_error("Failed to open shader file: " + file_path.string());
+  }
+
+  std::vector<char> buffer(file.tellg());
+
+  file.seekg(0, std::ios::beg);
+  file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+
+  return buffer;
+}
+
+
+} // namespace rf3d::gfx::vk
