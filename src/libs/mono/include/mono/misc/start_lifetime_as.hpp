12588024cf11ea9547ccd6867c3c39de3b9fea4b
ch0pe <acbsur1@gmail.com>
2026-02-22 22:48:25 +0100

    [Mono]: Fix start_lifetime_as implementation

--- src/libs/mono/include/mono/misc/start_lifetime_as.hpp
+++ src/libs/mono/include/mono/misc/start_lifetime_as.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
-#include <cstddef>
+#include <cstring>
 #include <new>
+#include <type_traits>
+
 
 namespace mono {
+
 
 /**
  * This is a replacement for std::start_lifetime_as_array from C++23
@@ -13,25 +16,27 @@ namespace mono {
  * since magic compiler tricks are not used here but is safer to use than reinterpret_cast.
  */
 template<typename T>
+  requires(std::is_trivially_copyable_v<T>)
 T* start_lifetime_as_array(void* p, std::size_t const n) noexcept {
-  auto* const bytes = new (p) std::byte[sizeof(T) * n]; // NOLINT
-  auto const ptr    = reinterpret_cast<T*>(bytes); // NOLINT
-  (void)*ptr;
-  return ptr;
+  std::memmove(p, p, sizeof(T) * n);
+  return std::launder(static_cast<T*>(p));
 }
 
 template<typename T>
+  requires(std::is_trivially_copyable_v<T>)
 T const* start_lifetime_as_array(void const* p, std::size_t const n) noexcept {
   auto* const mp = const_cast<void*>(p); // NOLINT
   return start_lifetime_as_array<T>(mp, n);
 }
 
 template<typename T>
+  requires(std::is_trivially_copyable_v<T>)
 T* start_lifetime_as(void* p) noexcept {
   return start_lifetime_as_array<T>(p, 1);
 }
 
 template<typename T>
+  requires(std::is_trivially_copyable_v<T>)
 T const* start_lifetime_as(void const* p) noexcept {
   return start_lifetime_as_array<T>(p, 1);
 }
