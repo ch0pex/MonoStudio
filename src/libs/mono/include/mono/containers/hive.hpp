9088927097b07ce81e7563eb3059ef4411c2b6a8
chope <100432481@alumnos.uc3m.es>
2026-03-25 00:52:29 +0100

    [CMake]: Change mvsc to use Ninja instead of Ninja Multi config

--- src/libs/mono/include/mono/containers/hive.hpp
+++ src/libs/mono/include/mono/containers/hive.hpp
@@ -1,8 +1,25 @@
 #pragma once
 
-#include <map>
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
+
 #include <plf_colony.h>
-#include <unordered_map>
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#elif defined(_MSC_VER)
+#pragma warning(pop)
+#endif
 
 namespace mono {
 
