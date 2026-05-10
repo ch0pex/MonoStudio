11f7d8d947ff3ddfd21ca8d15272ea1938e29211
ch0pe <acbsur1@gmail.com>
2026-05-10 21:42:08 +0200

    [Mono]: Improve mono::expected an mono::unexpected

--- src/libs/mono/include/mono/error/expected.hpp
+++ src/libs/mono/include/mono/error/expected.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
 #include <expected>
+#include <format>
 #include <stdexcept>
 
 namespace mono {
 
 template<typename T, typename Error = std::runtime_error>
-using expected = std::expected<T, std::runtime_error>;
+using expected = std::expected<T, Error>;
 
-inline std::unexpected<std::runtime_error> unexpected(std::string_view message) {
-  return std::unexpected<std::runtime_error> {std::runtime_error(std::string(message))};
+template<typename... Args>
+inline std::unexpected<std::runtime_error> unexpected(std::string_view message, Args const&... args) {
+  if constexpr (sizeof...(Args) == 0) {
+    return std::unexpected<std::runtime_error> {std::runtime_error(std::string(message))};
+  }
+
+  return std::unexpected<std::runtime_error> {
+    std::runtime_error(std::vformat(message, std::make_format_args(args...)))
+  };
 }
 
 } // namespace mono
