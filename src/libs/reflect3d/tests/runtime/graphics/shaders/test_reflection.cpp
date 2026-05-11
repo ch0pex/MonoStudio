764a894aa467ebda1c21dec1995bb4b0c376e733
ch0pe <acbsur1@gmail.com>
2026-05-11 22:46:39 +0200

    [Reflect3d]: Add basic shader reflection tests

--- src/libs/reflect3d/tests/runtime/graphics/shaders/test_reflection.cpp
+++ src/libs/reflect3d/tests/runtime/graphics/shaders/test_reflection.cpp
@@ -14,6 +14,7 @@
 // --- Dependencies ---
 
 #include <reflect3d/graphics/core/shader/compiler.hpp>
+#include <slang.h>
 
 // --- External dependencies ---
 
@@ -28,32 +29,39 @@
 
 TEST_SUITE_BEGIN("Shader");
 
-TEST_CASE("Test reflection") {
-
+TEST_CASE("Reflection exposes all global parameters with correct names") {
   using namespace rf3d::shader;
 
-  std::filesystem::path shader_file = std::filesystem::path {mono::assets_path} / "shaders/test_bindless_shader.slang";
-  auto const program                = compile<SpirV>(shader_file).value();
+  std::filesystem::path const shader_file =
+      std::filesystem::path {mono::assets_path} / "shaders/test_bindless_shader.slang";
+  auto const program = compile<SpirV>(shader_file).value();
 
   CHECK_FALSE(program.bytecode().empty());
 
-  auto parameters   = program.parameters();
-  auto entry_points = program.entry_points();
-
+  auto parameters = program.parameters();
   CHECK(std::ranges::distance(parameters) == 6);
   for (auto [idx, parameter]: parameters | std::views::enumerate) {
     LOG_INFO("Parameter {}: name='{}', type='{}'", idx, parameter->getName(), parameter->getType()->getName());
     CHECK(std::string {parameter->getName()} == std::format("gParameter{}", idx));
   }
+}
 
+TEST_CASE("Reflection exposes all entry points with correct names and stages") {
+  using namespace rf3d::shader;
+
+  std::filesystem::path const shader_file =
+      std::filesystem::path {mono::assets_path} / "shaders/test_bindless_shader.slang";
+  auto const program = compile<SpirV>(shader_file).value();
+
+  auto entry_points = program.entry_points();
   CHECK(std::ranges::distance(entry_points) == 2);
-  // for (auto [idx, entry_point]: entry_points | std::views::enumerate) {
-  // LOG_INFO("Entry Point {}: name='{}', stage={}", idx, entry_point->getName(), entry_point->getStage());
-  // CHECK(
-  //     std::string {entry_point->getName()} ==
-  //     std::format("{}Main", entry_point->getStage() == slang::ShaderStage::Vertex ? "vertex" : "fragment")
-  // );
-  // }
+  for (auto [idx, entry_point]: entry_points | std::views::enumerate) {
+    LOG_INFO("Entry point {}: name='{}'", idx, entry_point->getName());
+    CHECK(
+        std::string {entry_point->getName()} ==
+        std::format("{}Main", entry_point->getStage() == SLANG_STAGE_VERTEX ? "vert" : "frag")
+    );
+  }
 }
 
 TEST_SUITE_END();
